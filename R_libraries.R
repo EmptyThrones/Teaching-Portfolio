1f30d6a97cda77dad824c2c5158aa163f44276ac
Denzel <denzelavant@gmail.com>
2023-11-30 10:04:07 -0600

    Create R_libraries.R

---
+++ R_libraries.R
@@ -0,0 +1,3 @@
+my_packages <- c("tidyverse", "broom", "coefplot", "cowplot", "gapminder", "GGally", "ggrepel", "ggridges", "gridExtra", "here", "interplot", "margins", "maps", "mapproj", "mapdata", "MASS", "quantreg", "rlang", "scales", "survey", "srvyr", "virdis", "viridisLite", "devtools")
+install.packages(my_packages, repos = "http://cran.rstudio.com")
+devtools::install_github("kjhealy/socviz")
